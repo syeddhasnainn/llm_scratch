904fddadc6191a46e5f27ab9ac981c24fa816cd5
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-05-17 22:50:37 +0100

    self attention v1

--- attention.py
+++ attention.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn as nn
+
 inputs = torch.tensor(
   [[0.43, 0.15, 0.89], # Your     (x^1)
    [0.55, 0.87, 0.66], # journey  (x^2)
@@ -9,3 +11,31 @@ inputs = torch.tensor(
 )
 
 print(inputs.shape) #[6,3]
+
+x_2 = inputs[1]
+d_in = inputs.shape[1]
+d_out = 2
+
+class SelfAttention_v1(nn.Module):
+    def __init__(self, d_in, d_out):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_key   = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+ 
+    def forward(self, x):
+        keys = x @ self.W_key
+        queries = x @ self.W_query
+        values = x @ self.W_value
+        attn_scores = queries @ keys.T 
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1)
+        context_vec = attn_weights @ values
+        return context_vec
+
+
+torch.manual_seed(123)
+sa_v1 = SelfAttention_v1(d_in, d_out)
+print(sa_v1(inputs))
+    
