711b86fb139b20b25fd46ab66a785268dd01ceac
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-05-18 19:54:31 +0100

    self attention v2

--- attention.py
+++ attention.py
@@ -35,7 +35,24 @@ class SelfAttention_v1(nn.Module):
         return context_vec
 
 
-torch.manual_seed(123)
-sa_v1 = SelfAttention_v1(d_in, d_out)
-print(sa_v1(inputs))
+class SelfAttention_v2(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_score = queries @ keys.T
+        attn_weight = torch.softmax(attn_score / keys.shape[-1]**0.5, dim=1)
+        context_vector = attn_weight @ values
+        return context_vector
     
+torch.manual_seed(789)
+sa_v2 = SelfAttention_v2(d_in, d_out)
+print(sa_v2(inputs))
