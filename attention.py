a1b2b2efd3db03bc73f58dae0cb313fd3f9819f0
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-05-18 23:04:37 +0100

    causal attention with dropout

--- attention.py
+++ attention.py
@@ -56,3 +56,40 @@ class SelfAttention_v2(nn.Module):
 torch.manual_seed(789)
 sa_v2 = SelfAttention_v2(d_in, d_out)
 print(sa_v2(inputs))
+
+queries = sa_v2.W_query(inputs)
+keys = sa_v2.W_key(inputs)
+
+attn_scores = queries @ keys.T
+attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=1)
+print(attn_weights)
+print('shape:', attn_scores.shape)
+
+context_length = attn_scores.shape[0]
+
+mask_simple = torch.tril(torch.ones(context_length, context_length))
+print(mask_simple)
+
+
+masked_simple =  attn_weights * mask_simple
+print(masked_simple)
+
+row_sums = masked_simple.sum(dim=1, keepdim=True)
+masked_simple_norm = masked_simple / row_sums
+print(masked_simple_norm)
+
+mask = torch.triu(torch.ones(context_length, context_length), diagonal=1)
+print('mask', mask)
+masked = attn_scores.masked_fill(mask.bool(), -torch.inf)
+print(masked)
+
+attn_weights = torch.softmax(masked / keys.shape[-1] ** 0.5, dim=1)
+print(attn_weights)
+
+torch.manual_seed(123)
+dropout = torch.nn.Dropout(0.5)
+example = torch.ones(6,6)
+print(dropout(example))
+
+torch.manual_seed(123)
+print(dropout(attn_weights))
