c5556b3e9be0d3d3da479934fe82f31e7ea16b1e
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-05-19 16:06:17 +0100

    causal attention class

--- attention.py
+++ attention.py
@@ -93,3 +93,40 @@ print(dropout(example))
 
 torch.manual_seed(123)
 print(dropout(attn_weights))
+
+batch = torch.stack((inputs, inputs), dim=0)
+print(batch)
+
+class CausalAttention(nn.Module):
+    def __init__(self,d_in, d_out, context_length, dropout, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout)
+
+        self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length), diagonal=-1))
+    
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape  
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+ 
+        attn_scores = queries @ keys.transpose(1, 2)  
+        attn_scores.masked_fill_(self.mask.bool()[:num_tokens, :num_tokens], -torch.inf) 
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+ 
+        context_vec = attn_weights @ values
+        return context_vec
+
+
+
+torch.manual_seed(123)
+context_length = batch.shape[1]
+ca = CausalAttention(d_in, d_out,context_length, 0.0)
+cv = ca(batch)
+print('context_vecs shape:', cv.shape)
