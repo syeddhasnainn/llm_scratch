8e41f8f54a7f876e697957ff8b9082dbf4cb7520
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-05-19 23:09:25 +0100

    gpt dummy follow along

--- attention.py
+++ attention.py
@@ -152,8 +152,7 @@ print(cv)
 print(cv.shape)
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, d_in, d_out, 
-                 context_length, dropout, num_heads, qkv_bias=False):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
         super().__init__()
         assert d_out % num_heads == 0, "d_out must be divisible by num_heads"
  
@@ -165,41 +164,36 @@ class MultiHeadAttention(nn.Module):
         self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.out_proj = nn.Linear(d_out, d_out) #B
         self.dropout = nn.Dropout(dropout)
-        self.register_buffer(
-            'mask',
-             torch.triu(torch.ones(context_length, context_length), diagonal=1)
-        )
+        self.register_buffer('mask',torch.triu(torch.ones(context_length, context_length), diagonal=1))
  
     def forward(self, x):
         b, num_tokens, d_in = x.shape
-        keys = self.W_key(x) #C
-        queries = self.W_query(x) #C
-        values = self.W_value(x) #C
+        keys = self.W_key(x) 
+        queries = self.W_query(x) 
+        values = self.W_value(x) 
  
         keys = keys.view(b, num_tokens, self.num_heads, self.head_dim) #D
         values = values.view(b, num_tokens, self.num_heads, self.head_dim) #D
         queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)#D
  
-        keys = keys.transpose(1, 2) #E
-        queries = queries.transpose(1, 2) #E
-        values = values.transpose(1, 2) #E
+        keys = keys.transpose(1, 2) 
+        queries = queries.transpose(1, 2) 
+        values = values.transpose(1, 2) 
  
-        attn_scores = queries @ keys.transpose(2, 3)  #F 
-        mask_bool = self.mask.bool()[:num_tokens, :num_tokens] #G
+        attn_scores = queries @ keys.transpose(2, 3) 
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens] 
   
-        attn_scores.masked_fill_(mask_bool, -torch.inf) #H
+        attn_scores.masked_fill_(mask_bool, -torch.inf) 
  
-        attn_weights = torch.softmax(
-            attn_scores / keys.shape[-1]**0.5, dim=-1)
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
         attn_weights = self.dropout(attn_weights)
  
-        context_vec = (attn_weights @ values).transpose(1, 2) #I
-        #J
+        context_vec = (attn_weights @ values).transpose(1, 2) 
         context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
-        context_vec = self.out_proj(context_vec) #K
+        context_vec = self.out_proj(context_vec) 
         return context_vec
 
-a = torch.tensor([[[[0.2745, 0.6584, 0.2775, 0.8573], #A
+a = torch.tensor([[[[0.2745, 0.6584, 0.2775, 0.8573], 
                     [0.8993, 0.0390, 0.9268, 0.7388],
                     [0.7179, 0.7058, 0.9156, 0.4340]],
  
