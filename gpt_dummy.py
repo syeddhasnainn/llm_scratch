c18d88f7b995005a09c1e7a252582557f537709a
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-05-20 23:29:50 +0100

    gelu activation

--- gpt_dummy.py
+++ gpt_dummy.py
@@ -124,3 +124,32 @@ var = out_ln.var(dim=-1, keepdim=True)
 
 print('Mean:\n', mean)
 print('Var:\n', var)
+
+#Implementing a feed forward network with GELU activations
+
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+ 
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) * 
+            (x + 0.044715 * torch.pow(x, 3))
+        ))
+
+
+
+
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(nn.Linear(cfg['emb_dim'], 4 * cfg['emb_dim']), nn.Linear(4*cfg['emb_dim'], cfg['emb_dim']))
+
+    def forward(self, x):
+        return self.layers(x)
+    
+ffn = FeedForward(GPT_CONFIG_124M)
+x = torch.rand(2,3,768)
+out = ffn(x)
+print(out.shape)
