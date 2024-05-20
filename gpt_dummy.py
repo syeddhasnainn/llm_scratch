64f84c5f1910776e1431b983792bc75bb2ee8c73
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-05-20 01:26:40 +0100

    mean and variance

--- gpt_dummy.py
+++ gpt_dummy.py
@@ -56,3 +56,35 @@ class DummyLayerNorm(nn.Module):
 
     def forward(self, x):
         return x
+    
+import tiktoken
+
+tokenizer = tiktoken.get_encoding('gpt2')
+batch = []
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+
+#these texts are represented as 4 rows in the tensor
+
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch = torch.stack(batch, dim=0)
+print(batch)
+
+torch.manual_seed(123)
+model = DummyGPTModel(GPT_CONFIG_124M)
+logits = model(batch)
+print('output shape:', logits.shape)
+print(logits)
+
+torch.manual_seed(123)
+
+batch_example = torch.randn(2,5)
+layer = nn.Sequential(nn.Linear(5,6), nn.ReLU())
+out = layer(batch_example)
+print(out)
+
+mean = out.mean(dim=-1, keepdim=True)
+var = out.var(dim=-1, keepdim=True)
+print('mean:', mean)
+print('var:', var)
