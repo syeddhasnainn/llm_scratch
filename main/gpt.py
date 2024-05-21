b344184937d87ef29f18e264058aefacf3b797f4
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-05-21 22:40:20 +0100

    chap 4 completed

--- main/gpt.py
+++ main/gpt.py
@@ -148,3 +148,33 @@ print('total:', total_params)
 total_size_bytes = total_params * 4 
 total_size_mb = total_size_bytes / (1024 * 1024)
 print('size:', total_size_mb)
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:,-context_size:]
+
+        with torch.no_grad():
+            logits = model(idx_cond)
+            logits = logits[:, -1,:]
+            probas = torch.softmax(logits, dim=-1)
+            idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+            idx = torch.cat((idx, idx_next), dim=1)
+
+    return idx
+
+start_context = "Hello, I am"
+encoded = tokenizer.encode(start_context)
+print('encoded:', encoded)
+encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+print(encoded_tensor.shape)
+
+model.eval()
+out = generate_text_simple(model=model, 
+                           idx=encoded_tensor,
+                           max_new_tokens=6,
+                           context_size=GPT_CONFIG_124M['context_length'])
+print('output:', out)
+print(len(out[0]))
+
+decoded_text = tokenizer.decode(out.squeeze(0).tolist())
+print(decoded_text)
