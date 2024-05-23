899c6bcfb5a246ae9425a6e29809e5f12b34ad83
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-05-23 23:13:18 +0100

    token to text

--- main/evaluate.py
+++ main/evaluate.py
@@ -21,3 +21,16 @@ def text_to_token_ids(text, tokenizer):
     encoded_tensor = torch.tensor(encoded).unsqueeze(0)
     return encoded_tensor
 
+def token_ids_to_text(token_ids, tokenizer):
+    flat = token_ids.squeeze(0)
+    return tokenizer.decode(flat.tolist())
+
+start_context = "Every effort moves you"
+tokenizer = tiktoken.get_encoding('gpt2')
+
+token_ids = generate_text_simple(model=model, 
+                                 idx=text_to_token_ids(start_context, tokenizer),
+                                 max_new_tokens=10,
+                                 context_size=GPT_CONFIG_124M['context_length'])
+
+print('output text:\n', token_ids_to_text(token_ids, tokenizer))
