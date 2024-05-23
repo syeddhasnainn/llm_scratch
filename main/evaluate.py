e37f58e6ea11fce9cf11d2a7df77ec1f7a0322ff
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-05-23 22:48:28 +0100

    text to token

--- main/evaluate.py
+++ main/evaluate.py
@@ -1,15 +1,23 @@
 import torch
-from gpt import GPTModel
+from gpt import GPTModel, generate_text_simple
+import tiktoken
 
 GPT_CONFIG_124M = {
     "vocab_size": 50257,
-    "context_length": 256,  #A
+    "context_length": 256,  
     "emb_dim": 768,
     "n_heads": 12,
     "n_layers": 12, 
-    "drop_rate": 0.1,     #B
+    "drop_rate": 0.1,     
     "qkv_bias": False
 }
+
 torch.manual_seed(123)
 model = GPTModel(GPT_CONFIG_124M)
 model.eval()
+
+def text_to_token_ids(text, tokenizer):
+    encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+    return encoded_tensor
+
