a2132d8523e6c1402a527983e825e94264dfb042
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-05-28 19:02:48 +0100

    topk sampling

--- main/loader.py
+++ main/loader.py
@@ -158,9 +158,109 @@ def generate_and_print_sample(model, tokenizer, device, start_context):
         print(decoded_text.replace("\n", " "))
         model.train()
 
+# torch.manual_seed(123)
+# model = GPTModel(GPT_CONFIG_124M)
+# model.to(device)
+# optimizer = torch.optim.AdamW(model.parameters(), lr=0.0004, weight_decay=0.1)
+# num_epochs = 10
+# train_losses, val_losses, tokens_seen = train_model_simple(model, train_loader, val_loader, optimizer, device, num_epochs=num_epochs, eval_freq=5,eval_iter=1, start_context="Every effort moves you")
+
+model.to("cpu")
+model.eval()
+
+tokenizer = tiktoken.get_encoding('gpt2')
+token_ids = generate_text_simple(model=model,idx=text_to_token_ids("Every effort moves you",tokenizer),max_new_tokens=25, context_size=GPT_CONFIG_124M["context_length"])
+print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+vocab = { 
+    "closer": 0,
+    "every": 1, 
+    "effort": 2, 
+    "forward": 3,
+    "inches": 4,
+    "moves": 5, 
+    "pizza": 6,
+    "toward": 7,
+    "you": 8,
+}
+
+inverse_vocab = {v:k for k, v in vocab.items()}
+next_token_logits = torch.tensor(
+    [4.51, 0.89, -1.90, 6.75, 1.63, -1.62, -1.89, 6.28, 1.79]
+)
+
+probas = torch.softmax(next_token_logits, dim=0)
+next_token_id = torch.argmax(probas).item()
+print(inverse_vocab[next_token_id])
+
 torch.manual_seed(123)
-model = GPTModel(GPT_CONFIG_124M)
-model.to(device)
-optimizer = torch.optim.AdamW(model.parameters(), lr=0.0004, weight_decay=0.1)
-num_epochs = 10
-train_losses, val_losses, tokens_seen = train_model_simple(model, train_loader, val_loader, optimizer, device, num_epochs=num_epochs, eval_freq=5,eval_iter=1, start_context="Every effort moves you")
+next_token_id = torch.multinomial(probas, num_samples=1).item()
+print(inverse_vocab[next_token_id])
+
+def print_sample_tokens(probas):
+    torch.manual_seed(123)
+    sample = [torch.multinomial(probas, num_samples=1).item() for i in range(1000)]
+    sample_ids = torch.bincount(torch.tensor(sample))
+    for i, freq in enumerate(sample_ids):
+        print(f'{freq} x {inverse_vocab[i]}')
+
+print_sample_tokens(probas)
+
+def softmax_with_temperature(logits, temperature):
+    scaled_logits = logits / temperature
+    return torch.softmax(scaled_logits, dim=0)
+
+# import matplotlib.pyplot as plt
+
+# temperatures = [1, 0.1, 5]  
+# scaled_probas = [softmax_with_temperature(next_token_logits, T) for T in temperatures]
+# x = torch.arange(len(vocab))
+# bar_width = 0.15
+# fig, ax = plt.subplots(figsize=(5, 3))
+# for i, T in enumerate(temperatures):
+#     rects = ax.bar(x + i * bar_width, scaled_probas[i], 
+#                    bar_width, label=f'Temperature = {T}')
+# ax.set_ylabel('Probability')
+# ax.set_xticks(x)
+# ax.set_xticklabels(vocab.keys(), rotation=90)
+# ax.legend()
+# plt.tight_layout()
+# plt.show()
+
+#top-k sampling
+
+top_k = 3
+top_logits, top_pos = torch.topk(next_token_logits, top_k)
+print('top logits', top_logits)
+print('top position', top_pos)
+
+new_logits = torch.where(condition=next_token_logits < top_logits[-1], input=torch.tensor(float('-inf')), other=next_token_logits)
+print(new_logits)
+
+topk_probas = torch.softmax(new_logits, dim=0)
+print(topk_probas)
+
+def generate_modified(model, idx, max_new_tokens, context_size, temperature, top_k=None):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        logits = logits[:,-1,:]
+        if top_k is not None:
+            top_logits, _ = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(logits < min_val, torch.tensor(float('-inf')).to(device), logits)
+            if temperature > 0.0:
+                logits = logits / temperature
+                probas = torch.softmax(logits, dim=-1)
+                idx_next = torch.multinomial(probas, num_samples=1)
+            else:
+                idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+                idx = torch.cat((idx, idx_next), dim=1)
+            
+    return idx
+
+torch.manual_seed(123)
+token_ids = generate_modified(model=model, idx=text_to_token_ids("Every effort moves you", tokenizer),max_new_tokens=15, context_size=GPT_CONFIG_124M['context_length'], top_k=25, temperature=1.4)
+print("output text:\n", token_ids_to_text(token_ids, tokenizer))
